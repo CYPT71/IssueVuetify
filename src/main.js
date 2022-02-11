4301a5e2252a4ca6497c28970c29f60ea55b2dde
cypt <cyprientaib1@gmail.com>
2022-02-11 16:38:32 +0100

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
+import sw from "./registerServiceWorker"
+
+
 loadFonts()
 
 createApp(App)
