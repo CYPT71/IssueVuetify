a8c1113f64e1a2adcf46cf12d454824cfa815da0
cypt <cyprientaib1@gmail.com>
2022-02-11 16:31:00 +0100

    update

---
+++ monopoly/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  },
+
+  
+
+  pwa: {
+    name: 'Monopoly',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
+    }
+  }
+}
